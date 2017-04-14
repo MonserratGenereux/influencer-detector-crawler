af03620d99e451dc6080f727b6f0d1ce130aad6b
Diego Toledo <toledo.diego09@gmail.com>
2017-04-14 12:56:22 -0500

    Implement Crawler logic for fetching Facebook pages

--- vendor/models/edges.go
+++ vendor/models/edges.go
@@ -2,4 +2,8 @@ package models
 
 // Edge defines a model to represent an edge in the social graph.
 type Edge struct {
+	// Source Node id
+	Source string
+	// Destination Node id
+	Destination string
 }
--- vendor/models/nodes.go
+++ vendor/models/nodes.go
@@ -2,4 +2,21 @@ package models
 
 // Node defines a model to represent an node in the social graph.
 type Node struct {
+	// Page info
+	Name        string
+	Description string
+	ID          string
+
+	// Relevance
+	Category     string
+	CategoryList []string
+	FanCount     int64
+
+	// Location
+	City    string
+	Country string
+	ZIP     int64
+
+	// Developers controll
+	Fetched bool
 }
